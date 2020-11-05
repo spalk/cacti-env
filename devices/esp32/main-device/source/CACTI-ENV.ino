8938d1958adaf0c4929ef14201dc65e3060df3c0
demetrius <d.natxa@gmail.com>
2020-11-05 11:31:28 +0300

    try on production

--- devices/esp32/main-device/source/CACTI-ENV.ino
+++ devices/esp32/main-device/source/CACTI-ENV.ino
@@ -33,7 +33,7 @@ int P, H, L_alfa, L_beta, S_alfa_volt, S_beta_volt, S_alfa_perc, S_beta_perc;
 unsigned long time_to_refresh_display = 0;
 const long refresh_display_interval = 1000;
 unsigned long time_to_send_data = 0;
-const long send_data_interval = 10000;
+const long send_data_interval = 60000; // send data to broker every 1 min
 unsigned long time_to_switch_bckl = 0;
 const long switch_bckl_interval = 500;
 
@@ -48,14 +48,12 @@ bool its_time_to_switch_bckl();
 String IpAddress2String(const IPAddress& ipAddress);
 
 
-
 void setup() {
   Serial.begin(115200);
 
   // display init
   display.init();
   display.turn_backlight_on();
-
   display.show_msg_in_line("LOADING:", 0);
   delay(500);
 
@@ -66,24 +64,19 @@ void setup() {
   led.blink_right(3, 100);
   delay(500);
 
-  display.show_msg_in_line("WiFi", 2);
-
   // OTA (Over The Air update) setup
+  display.show_msg_in_line("WiFi", 2);
   setupOTA(OTA_DEVICE_ID);
-
   display.show_loading_line("WiFi", "OK", 2);
   display.show_msg_in_line(IpAddress2String(WiFi.localIP()), 3);
 
   // MQTT
   mqtt.init();
-
   display.show_loading_line("MQTT", "OK", 4);
 
-
+  // sensors init and get status
   display.clear();
   display.show_msg_in_line("Check sensors:", 0);
-
-  // sensors init and get status
   sensors.init();
   status_T_in  = sensors.get_T_in_status();
   status_T_out = sensors.get_T_out_status();
@@ -100,20 +93,18 @@ void setup() {
   display.show_loading_line("Humidity", (status_H) ? "OK" : "NOK", 4);
   display.clear();
 
-
   display.show_msg_in_line("Check sensors:", 0);
   display.show_loading_line("Light 1", (status_L_alfa) ? "OK" : "NOK", 1);
   display.show_loading_line("Light 2", (status_L_beta) ? "OK" : "NOK", 2);
   display.show_loading_line("Soil 1", (status_S_alfa) ? "OK" : "NOK", 3);
   display.show_loading_line("Soil 2", (status_S_beta) ? "OK" : "NOK", 4);
-
   delay(500);
 
   // show welcome screen
   display.show_welcome_screen();
   delay(3000);
 
-  // button
+  // button init
   pinMode(BUTTON_PIN, INPUT);
 }
 
@@ -144,7 +135,6 @@ void loop() {
       display.switch_backlight();
     }
   }
-
 }
 
 
@@ -188,6 +178,7 @@ bool its_time_to_send_data(){
   }
   return answer;
 }
+
 
 bool its_time_to_switch_bckl(){
   bool answer;
